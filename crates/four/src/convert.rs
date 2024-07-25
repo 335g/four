e74d6399938ab21519df6fb2b6ec87e70dda2cf8
335g <actionstar619@yahoo.co.jp>
2024-07-26 01:26:49 +0900

    chore: Box -> WillBe

--- crates/four/src/convert.rs
+++ crates/four/src/convert.rs
@@ -48,6 +48,18 @@ where
     }
 }
 
+impl<T> From<Box<T>> for WillBe<T>
+where
+    T: WillFrom + 'static,
+{
+    fn from(value: Box<T>) -> Self {
+        WillBe {
+            from: value,
+            to: PhantomData,
+        }
+    }
+}
+
 impl<T> Serialize for WillBe<T> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
