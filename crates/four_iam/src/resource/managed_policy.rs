e8a9e70d30087503e2c31b7ad4c9681788abe6be
335g <actionstar619@yahoo.co.jp>
2024-07-29 21:38:46 +0900

    feat: add AWSManagedPolicy (lambda)

--- crates/four_iam/src/resource/managed_policy.rs
+++ crates/four_iam/src/resource/managed_policy.rs
@@ -16,18 +16,59 @@ pub struct CustomerManagedPolicy {
 #[derive(Debug, Clone, Serialize)]
 pub struct AWSManagedPolicy(Arn<IAM>);
 
-impl AWSManagedPolicy {
-    /// LambdaBasicExecutionRole
-    /// cf. https://docs.aws.amazon.com/ja_jp/aws-managed-policy/latest/reference/AWSLambdaBasicExecutionRole.html
-    pub fn lambda_basic_execution_role() -> Self {
-        let arn = Arn::builder(
-            IAM,
-            "policy/service-role/AWSLambdaBasicExecutionRole",
-            Account::Aws,
-        )
-        .partition(Partition::Ref)
-        .build();
-
-        AWSManagedPolicy(arn)
-    }
+macro_rules! aws_managed_policy {
+    ($(($name:ident, $resource:expr)),*) => {
+        impl AWSManagedPolicy {
+            $(pub fn $name() -> Self {
+                let arn = Arn::builder(IAM, $resource, Account::Aws)
+                    .partition(Partition::Ref)
+                    .build();
+                AWSManagedPolicy(arn)
+            })*
+        }
+    };
 }
+
+aws_managed_policy!(
+    (lambda_full_access, "policy/AWSLambda_FullAccess"),
+    (lambda_read_only_access, "policy/AWSLambda_ReadOnlyAccess"),
+    (
+        lambda_basic_execution_role,
+        "policy/service-role/AWSLambdaBasicExecutionRole"
+    ),
+    (
+        lambda_dynamo_db_execution_role,
+        "policy/service-role/AWSLambdaDynamoDBExecutionRole"
+    ),
+    (
+        lambda_eni_management_access,
+        "policy/service-role/AWSLambdaENIManagementAccess"
+    ),
+    (lambda_execute, "policy/AWSLambdaExecute"),
+    (lambda_full_access, "policy/AWSLambdaFullAccess"),
+    (
+        lambda_invocation_dynamo_db,
+        "policy/AWSLambdaInvocation-DynamoDB"
+    ),
+    (
+        lambda_kinesis_execution_role,
+        "policy/service-role/AWSLambdaKinesisExecutionRole"
+    ),
+    (
+        lambda_msk_execution_role,
+        "policy/service-role/AWSLambdaMSKExecutionRole"
+    ),
+    (
+        lambda_replicator,
+        "policy/aws-service-role/AWSLambdaReplicator"
+    ),
+    (lambda_role, "policy/service-role/AWSLambdaRole"),
+    (
+        lambda_sqs_queue_execution_role,
+        "policy/service-role/AWSLambdaSQSQueueExecutionRole"
+    ),
+    (
+        lambda_vpc_access_execution_role,
+        "policy/service-role/AWSLambdaVPCAccessExecutionRole"
+    )
+);
