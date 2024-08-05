ef188ae753358f3c1415309556ea04e809ae569d
335g <actionstar619@yahoo.co.jp>
2024-08-05 21:06:25 +0900

    feat: instance profile

--- crates/four_iam/src/resource/instance_profile.rs
+++ crates/four_iam/src/resource/instance_profile.rs
@@ -1,22 +1,68 @@
 use four::{
+    arn::Arn,
     convert::{WillBe, WillMappable},
+    function::{
+        getatt::{Attribute, HaveAtt},
+        reference::Referenced,
+    },
     logical_id::LogicalId,
+    service::IAM,
     ManagedResource,
 };
 use serde::Serialize;
 
-use super::role::RoleArn;
+use crate::resource::role::RoleName;
 
 #[derive(ManagedResource, Clone)]
 #[resource_type = "AWS::IAM::InstanceProfile"]
 pub struct InstanceProfile {
     logical_id: LogicalId,
-    instance_profile_name: WillBe<InstanceProfileName>,
-    path: String,
-    roles: Vec<WillBe<RoleArn>>,
+    instance_profile_name: Option<WillBe<InstanceProfileName>>,
+    path: Option<String>,
+    roles: Vec<WillBe<RoleName>>,
+}
+
+impl InstanceProfile {
+    pub fn new(logical_id: LogicalId, roles: Vec<WillBe<RoleName>>) -> InstanceProfile {
+        InstanceProfile {
+            logical_id,
+            instance_profile_name: None,
+            path: None,
+            roles,
+        }
+    }
+
+    pub fn instance_profile_name(mut self, name: WillBe<InstanceProfileName>) -> InstanceProfile {
+        self.instance_profile_name = Some(name);
+        self
+    }
+
+    pub fn path(mut self, path: String) -> InstanceProfile {
+        self.path = Some(path);
+        self
+    }
 }
 
 #[derive(Debug, Clone, Serialize)]
 pub struct InstanceProfileName(String);
 
 impl WillMappable<String> for InstanceProfileName {}
+
+impl Referenced for InstanceProfile {
+    type To = InstanceProfileName;
+
+    fn referenced(&self) -> four::function::reference::RefInner {
+        four::function::reference::RefInner::Id(self.logical_id.clone())
+    }
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct InstanceProfileArn(Arn<IAM>);
+
+impl Attribute for InstanceProfileArn {
+    fn name() -> &'static str {
+        "Arn"
+    }
+}
+
+impl HaveAtt<InstanceProfileName> for InstanceProfile {}
