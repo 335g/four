5e25d22f4b39bc4998ffca6c7c0e0666384e01b2
335g <actionstar619@yahoo.co.jp>
2024-08-14 20:47:42 +0900

    feat: service Action

--- four/src/iam/property/action.rs
+++ four/src/iam/property/action.rs
@@ -6,14 +6,20 @@ erased_serde::serialize_trait_object!(Action);
 dyn_clone::clone_trait_object!(Action);
 
 macro_rules! impl_action {
-    ( $service:ident, [$($t:ident),*] ) => {
+    ( $service:ident, $action:ident, [$($t:ident),*] ) => {
         pub mod $service {
             use serde::Serialize;
+
+            pub trait $action: super::Action {}
+            erased_serde::serialize_trait_object!($action);
+            dyn_clone::clone_trait_object!($action);
+
             $(
             #[derive(Debug, Clone, Copy)]
             pub struct $t;
 
             impl super::Action for $t {}
+            impl $action for $t {}
 
             impl Serialize for $t {
                 fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -28,4 +34,77 @@ macro_rules! impl_action {
     };
 }
 
-impl_action!(sts, [AssumeRole]);
+impl_action!(sts, StsAction, [AssumeRole]);
+impl_action!(
+    lambda,
+    LambdaAction,
+    [
+        AddLayerVersionPermission,
+        AddPermission,
+        CreateAlias,
+        CreateCodeSigningConfig,
+        CreateEventSourceMapping,
+        CreateFunction,
+        CreateFunctionUrlConfig,
+        DeleteAlias,
+        DeleteCodeSigningConfig,
+        DeleteEventSourceMapping,
+        DeleteFunction,
+        DeleteFunctionCodeSigningConfig,
+        DeleteFunctionConcurrency,
+        DeleteFunctionEventInvokeConfig,
+        DeleteFunctionUrlConfig,
+        DeleteLayerVersion,
+        DeleteProvisionedConcurrencyConfig,
+        DisableReplication,
+        EnableReplication,
+        GetAccountSettings,
+        GetAlias,
+        GetCodeSigningConfig,
+        GetEventSourceMapping,
+        GetFunction,
+        GetFunctionCodeSigningConfig,
+        GetFunctionConcurrency,
+        GetFunctionConfiguration,
+        GetFunctionEventInvokeConfig,
+        GetFunctionUrlConfig,
+        GetLayerVersion,
+        GetLayerVersionPolicy,
+        GetPolicy,
+        GetProvisionedConcurrencyConfig,
+        GetRuntimeManagementConfig,
+        InvokeAsync,
+        InvokeFunction,
+        InvokeFunctionUrl,
+        ListAliases,
+        ListCodeSigningConfigs,
+        ListEventSourceMappings,
+        ListFuctionEventInvokeConfigs,
+        ListFunctionUrlConfigs,
+        ListFunctions,
+        ListFunctionsByCodeSigningConfig,
+        ListLayerVersions,
+        ListLayers,
+        ListProvisionedConcurrencyConfigs,
+        ListTags,
+        ListVersionsByFunction,
+        PublishLayerVersion,
+        PutFunctionCodeSigningConfig,
+        PutFunctionConcurrency,
+        PutFunctionEventInvokeConfig,
+        PutProvisionedConcurrencyConfig,
+        PutRuntimeManagementConfig,
+        RemoveLayerVersionPermission,
+        RemovePermission,
+        TagResource,
+        UntagResource,
+        UpdateAlias,
+        UpdateCodeSigningConfig,
+        UpdateEventSourceMapping,
+        UpdateFunctionCode,
+        UpdateFunctionCodeSigningConfig,
+        UpdateFunctionConfiguration,
+        UpdateFunctionEventInvokeConfig,
+        UpdateFunctionUrlConfig
+    ]
+);
