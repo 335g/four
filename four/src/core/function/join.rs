9954a52189bec7a6473fd8e003461722bb2b83ef
335g <actionstar619@yahoo.co.jp>
2024-08-18 04:29:23 +0900

    refactor: remove fn_join macro

--- four/src/core/function/join.rs
+++ four/src/core/function/join.rs
@@ -27,22 +27,6 @@ impl Referenced for Join {
     }
 }
 
-#[macro_export]
-macro_rules! fn_join {
-    ($join:expr, [$($elem:expr),+]) => {
-        {
-            let mut all_elements = $join.0;
-            let mut new_elements: Vec<Box<dyn erased_serde::Serialize>> = vec![$(Box::new($elem)),*];
-            all_elements.append(&mut new_elements);
-            Join(all_elements)
-        }
-    };
-
-    ($($elem:expr),+) => {
-        Join::new(vec![$(Box::new($elem)),*])
-    };
-}
-
 impl Serialize for Join {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
