a07c18771bc8fabd4bcf88649daa406b39421081
335g <actionstar619@yahoo.co.jp>
2024-08-18 01:18:48 +0900

    refactor: lambda::property

--- four/src/lambda/mod.rs
+++ four/src/lambda/mod.rs
@@ -1,2 +1,10 @@
-pub mod property;
+mod property;
 pub mod resource;
+
+pub use property::{
+    architecture::{Architecture, Architectures},
+    code::Code,
+    function_name::FunctionName,
+    handler::{Handler, HandlerError},
+    runtime::Runtime,
+};
